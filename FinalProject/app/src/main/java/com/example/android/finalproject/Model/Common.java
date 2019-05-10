cce352652d90b69673ac8d4154f850b0c8cc682c
Ernest <arreern@gmail.com>
2019-05-10 11:12:10 -0700

    Updated Quiz

---
+++ FinalProject/app/src/main/java/com/example/android/finalproject/Model/Common.java
@@ -0,0 +1,9 @@
+package com.example.android.finalproject.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Common {
+    public static User currentUser;
+    public static List<Question> questionList = new ArrayList();
+}
---
+++ FinalProject/app/src/main/java/com/example/android/finalproject/ResultsActivity.java
@@ -0,0 +1,62 @@
+package com.example.android.finalproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.android.finalproject.Controller.Home;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ResultsActivity extends AppCompatActivity {
+
+    Button btnTryAgain;
+    TextView txtResultScore, getTxtResultQuestion;
+    ProgressBar progressBar;
+
+    FirebaseDatabase database;
+    DatabaseReference question_score;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+
+        database = FirebaseDatabase.getInstance();
+        question_score = database.getReference("Question_Score");
+
+        txtResultScore = (TextView)findViewById(R.id.txtTotalScore);
+        getTxtResultQuestion = (TextView)findViewById(R.id.txtTotalQuestion);
+        progressBar = (ProgressBar)findViewById(R.id.doneProgressBar);
+        btnTryAgain = (Button) findViewById(R.id.btnTryAgain);
+
+        btnTryAgain.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(ResultsActivity.this, Home.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        Bundle extra = getIntent().getExtras();
+        if(extra != null){
+            int score = extra.getInt("SCORE");
+            int totalQuestion = extra.getInt("TOTAL");
+            int correctAnswer = extra.getInt("CORRECT");
+
+            txtResultScore.setText(String.format("SCORE : %d", score));
+            getTxtResultQuestion.setText(String.format("CORRECT: %d / %d", correctAnswer, totalQuestion));
+
+            progressBar.setMax(totalQuestion);
+            progressBar.setProgress(correctAnswer);
+
+
+
+        }
+    }
+}
